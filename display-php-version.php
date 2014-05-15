ac2a150745592b8f9560d15740169e9644a2095d
dgwyer <dgwyer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-15 16:20:05 +0000

    updated to v1.2
    git-svn-id: https://plugins.svn.wordpress.org/display-php-version/trunk@914742 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- display-php-version.php
+++ display-php-version.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Display PHP Version
 Description: Displays the current PHP version in the "At a Glance" admin dashboard widget.
-Version: 1.1
+Version: 1.2
 Author: David Gwyer
 Author URI: http://www.presscoders.com
 */
@@ -24,13 +24,19 @@ Author URI: http://www.presscoders.com
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function pc_add_php_version() {
-	?>
-	<script language="javascript">
-		jQuery(document).ready(function($) {
-			$("#wp-version-message").after("<p>PHP version: <b><?php echo phpversion(); ?></b></p>");
-		});
-	</script>
-	<?php
+function dpv_enqueue_script($hook) {
+
+	// only run on dashboard page
+	if( 'index.php' != $hook )
+        return;
+
+	// enqueue script to show PHP version
+	wp_enqueue_script( 'dpv_script', plugin_dir_url( __FILE__ ) . 'dpv.js' );
+
+	// pass the PHP version to JavaScript
+	wp_localize_script( 'dpv_script', 'dpvObj', array(
+		'phpVersion' => phpversion()
+	) );
+
 }
-add_action('rightnow_end', 'pc_add_php_version');
+add_action( 'admin_enqueue_scripts', 'dpv_enqueue_script' );
---
+++ dpv.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+	$("#wp-version-message").after("<p>Running PHP version: <b>" + dpvObj.phpVersion + "</b></p>");
+});
